e4747f744bbbaeddb2b46feb7e9fe0d2bcf3b079
rihacek <jrihacek@gmail.com>
2024-09-21 19:32:15 -0400

    add sample rom

--- place-files.sh
+++ place-files.sh
@@ -3,3 +3,6 @@
 #EmulationStation config - default location
 cp es_systems.cfg /etc/emulationstation
 #todo: do we need to copy to /opt/retropie/configs/all/emulationstation as well?
+
+#sample rom for testing
+cp 'Duck Tales.zip' ~/RetroPie/roms/nes/
