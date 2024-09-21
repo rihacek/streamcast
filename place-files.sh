5fec1afd0bff0106691a475cf963de0e1f914f53
rihacek <jrihacek@gmail.com>
2024-09-21 19:42:24 -0400

    correct sudo user path issue

--- place-files.sh
+++ place-files.sh
@@ -5,4 +5,4 @@ cp es_systems.cfg /etc/emulationstation
 #todo: do we need to copy to /opt/retropie/configs/all/emulationstation as well?
 
 #sample rom for testing
-cp 'Duck Tales.zip' ~/RetroPie/roms/nes/
+cp 'Duck Tales.zip' /home/$USER/RetroPie/roms/nes/
