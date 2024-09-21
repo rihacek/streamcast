701468dc64b7c7d391cb4d364371d6296b368738
rihacek <jrihacek@gmail.com>
2024-09-21 11:04:39 -0400

    add plex

---
+++ place-files.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#EmulationStation config - default location
+cp es_systems.cfg /etc/emulationstation
+#todo: do we need to copy to /opt/retropie/configs/all/emulationstation as well?
